038d6270fd475fe75e3e176ba5faaed2d8b45c42
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2014-07-07 15:48:51 +0200

    Application outline

--- server/public/javascripts/app.js
+++ server/public/javascripts/app.js
@@ -1,3 +1,27 @@
-(function () {
-  
-})();
+(function (exports) {
+  var app = {components: {}};
+
+  app.components.App = React.createClass({
+    render: function () {
+      return React.DOM.h1({}, 'APP');
+    }
+  });
+
+  app.components.List = React.createClass({
+    render: function () {
+      return '';
+    }
+  });
+
+  app.components.Player = React.createClass({
+    render: function () {
+      return '';
+    }
+  });
+
+  app.initialize = function initialize(root) {
+    React.renderComponent(app.components.App({}), root);
+  };
+
+  exports.app = app;
+})(this);
