555924535f21a6e3ecc41a4f9fbaa0f92b8034e8
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2014-07-08 11:10:17 +0200

    Application outline

--- server/public/javascripts/app.js
+++ server/public/javascripts/app.js
@@ -3,13 +3,34 @@
 
   app.components.App = React.createClass({
     render: function () {
-      return React.DOM.h1({}, 'APP');
+      return React.DOM.div({}, [
+        app.components.List({})
+      ]);
     }
   });
 
   app.components.List = React.createClass({
+    getInitialState: function () {
+      return {conferences: []};
+    },
+    componentDidMount: function () {
+      $.get('/api/conferences.json', function (conferences) {
+        this.setState({conferences: conferences});
+      }.bind(this));
+    },
     render: function () {
-      return '';
+      return React.DOM.div({}, [
+        React.DOM.h2({}, 'Recorded Conferences'),
+        React.DOM.ul({}, this.state.conferences.map(function (c) {
+          return app.components.ListItem({conference: c});
+        }))
+      ]);
+    }
+  });
+
+  app.components.ListItem = React.createClass({
+    render: function () {
+      return React.DOM.li({}, React.DOM.a({}, this.props.conference.id));
     }
   });
 
